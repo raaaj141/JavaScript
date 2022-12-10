34a81ef09f96d40191c78e9bf76220a4e6c8e58b
raaaj141 <118422037+raaaj141@users.noreply.github.com>
2022-12-10 08:15:46 +0530

    JavaScript

---
+++ 8thDec/JAVAscript.js
@@ -0,0 +1,4 @@
+function myfunc(a,b){
+console.log(a+b);
+}
+myfunc(6,10);
---
+++ 8thDec/main.js
@@ -0,0 +1,6 @@
+function sum(a,b,c)
+{
+    hello=a+b+c
+    console.log(hello)
+}
+sum("hello ","Everyone ","i am mugil")
